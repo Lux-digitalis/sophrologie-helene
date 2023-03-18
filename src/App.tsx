8fa7b88f091f5c42b678dfafee904b9198fc207c
Beladric <kilian.goetz@yahoo.fr>
2023-03-18 11:40:53 +0100

    Navigation bar + footer
    I add navigation bar and footer, it missing some animation currently in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 function App(): JSX.Element {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -3,7 +3,13 @@ import navbarStyle from "./Navbar.module.sass";
 import Logo from "../../Assets/Logo.svg";
 
 export default function Navbar(): JSX.Element {
-  const SECTIONS = [
+  const [IS_CHECK, SetCheck] = React.useState<boolean>(false);
+
+  const TEST = () => {
+    SetCheck((currentValue) => !currentValue);
+  };
+
+  const SECTIONS: Array<{name: string; href: string}> = [
     {name: "Hélène Grass", href: ""},
     {name: "La thérapie", href: ""},
     {name: "Mes qualifications", href: ""},
@@ -13,13 +19,33 @@ export default function Navbar(): JSX.Element {
   ];
 
   return (
-    <div className={navbarStyle.navbar}>
-      <img src={Logo} alt="Logo" className={navbarStyle.logo} />
-      {SECTIONS.map((section, key) => (
-        <a href={section.href} key={key} className={navbarStyle.listElement}>
-          {section.name}
-        </a>
-      ))}
+    <div id={navbarStyle.navbar}>
+      <div id={navbarStyle.navigation}>
+        <img src={Logo} alt="Logo" className={navbarStyle.logo} />
+        {SECTIONS.map((section, key) => (
+          <a href={section.href} key={key} className={navbarStyle.listElement}>
+            {section.name}
+          </a>
+        ))}
+        <input type="checkbox" id={navbarStyle.burgerCheck} onClick={TEST} />
+        <div className={navbarStyle.burgerLines}>
+          {[...Array(3)].map((e, i) => (
+            <span className={navbarStyle.burgerLine} key={i}></span>
+          ))}
+        </div>
+      </div>
+      {IS_CHECK
+        ? SECTIONS.slice(1).map((section, key) => (
+            <a
+              href={section.href}
+              key={key}
+              className={navbarStyle.listElement}
+              style={{display: "flex", margin: "0 auto 0 auto"}}
+            >
+              {section.name}
+            </a>
+          ))
+        : null}
     </div>
   );
 }
